3aa7688e44f8a08b7f2e4a82336a2bf626ea5c3f
Cedric Kornacki <e11910525@student.tuwien.ac.at>
2022-03-22 16:39:34 +0100

    BodyForceMap done, BodyQueue errors occured

--- src/BodyForceMap.java
+++ src/BodyForceMap.java
@@ -4,12 +4,17 @@
 public class BodyForceMap {
 
     //TODO: declare variables.
+    private int top;
+    private Body[] ks;
+    private Vector3[] vs;
 
     // Initializes this map with an initial capacity.
     // Precondition: initialCapacity > 0.
     public BodyForceMap(int initialCapacity) {
 
         //TODO: define constructor.
+        this.ks = new Body[initialCapacity];
+        this.vs = new Vector3[initialCapacity];
     }
 
     // Adds a new key-value association to this map. If the key already exists in this map,
@@ -18,7 +23,21 @@ public class BodyForceMap {
     public Vector3 put(Body key, Vector3 force) {
 
         //TODO: implement method.
-        return null;
+        int i = find(key, ks);
+        if (i == top && ++top == ks.length) {
+            Body[] nks = new Body[top << 1];
+            Vector3[] nvs = new Vector3[top << 1];
+            for (int j = 0; j < i; j++) {
+                nks[j] = ks[j];
+                nvs[j] = vs[j];
+            }
+            ks = nks;
+            vs = nvs;
+        }
+        ks[i] = key;
+        Vector3 old = vs[i];
+        vs[i] = force;
+        return old;
     }
 
     // Returns the value associated with the specified key, i.e. the returns the force vector
@@ -27,6 +46,15 @@ public class BodyForceMap {
     public Vector3 get(Body key) {
 
         //TODO: implement method.
-        return null;
+        return vs[find(key, ks)];
     }
+
+    private int find(Body s, Body[] a) {
+        int i = 0;
+        while (i < top &&
+                !(s == null ? s == a[i] : s.equals(a[i])))
+            i++;
+        return i;
+    }
+
 }
--- src/BodyQueue.java
+++ src/BodyQueue.java
@@ -8,7 +8,6 @@ public class BodyQueue {
     //TODO: declare variables.
     private int initialCapacity;
     private DEQueue q;
-    private BodyQueue copyq;
 
     // Initializes this queue with an initial capacity.
     // Precondition: initialCapacity > 0.
@@ -24,7 +23,9 @@ public class BodyQueue {
     // Precondition: q != null.
     public BodyQueue(BodyQueue q) {
         //TODO: define constructor.
-        this.copyq = q;
+        this.initialCapacity = q.getInitialCapacity();
+        this.q = new DEQueue();
+        setQ(q.getQ());
     }
 
     // Adds the specified body 'b' to this queue.
@@ -38,7 +39,7 @@ public class BodyQueue {
     public Body poll() {
 
         //TODO: implement method.
-        return q.pollFirst();
+        return this.q.pollFirst();
     }
 
     // Returns the number of bodies in this queue.
@@ -47,24 +48,35 @@ public class BodyQueue {
         //TODO: implement method.
         return q.size();
     }
+
+    public int getInitialCapacity() {
+        return initialCapacity;
+    }
+
+    public DEQueue getQ() {
+        return q;
+    }
+    public void setQ(DEQueue q){
+        this.q = q;
+    }
 }
 
 class DEQueue{
     private int mask = (1 << 3) - 1;
-    private Body[] es = new Body[mask + 1];
+    private Body[] bodies = new Body[mask + 1];
     private int head, tail;
 
 
     public void addFirst(Body e) {
-        es[head = (head - 1) & mask] = e;
+        bodies[head = (head - 1) & mask] = e;
         if (tail == head) {
             doubleCapacity();
         }
     }
 
     public Body pollFirst() {
-        Body result = es[head];
-        es[head] = null;
+        Body result = bodies[head];
+        bodies[head] = null;
         if (tail != head) {
             head = (head + 1) & mask;
         }
@@ -72,11 +84,11 @@ class DEQueue{
     }
 
     public Body peekFirst() {
-        return es[head];
+        return bodies[head];
     }
 
     public void addLast(Body e) {
-        es[tail] = e;
+        bodies[tail] = e;
         tail = (tail + 1) & mask;
         if (tail == head) {
             doubleCapacity();
@@ -87,13 +99,13 @@ class DEQueue{
         if (tail != head) {
             tail = (tail - 1) & mask;
         }
-        Body result = es[tail];
-        es[tail] = null;
+        Body result = bodies[tail];
+        bodies[tail] = null;
         return result;
     }
 
     public Body peekLast() {
-        return es[(tail - 1) & mask];
+        return bodies[(tail - 1) & mask];
     }
 
     public int size() {
@@ -105,12 +117,12 @@ class DEQueue{
         Body[] newes = new Body[mask + 1];
         int i = 0, j = 0;
         while (i < head) {
-            newes[j++] = es[i++];
+            newes[j++] = bodies[i++];
         }
-        j = head += es.length;
-        while (i < es.length) {
-            newes[j++] = es[i++];
+        j = head += bodies.length;
+        while (i < bodies.length) {
+            newes[j++] = bodies[i++];
         }
-        es = newes;
+        bodies = newes;
     }
 }
